5910153f577627dcfcedb0acd94daac3723ccc49
RichardJhn <rjohn@my.yearupunited.org>
2025-11-04 11:53:43 -0500

    added lamba for first and last name search

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -1,38 +1,46 @@
 package com.pluralsight;
 
 import javax.crypto.spec.PSource;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Locale;
-import java.util.Scanner;
+import java.util.*;
+import java.util.stream.Collectors;
+
 public class Main {
     public static void main (String[] Args) {
 
         Scanner scanner = new Scanner(System.in);
-        List<Person> people = new ArrayList<>();
-        people.add(new Person("Richard", "John", 22));
-        people.add(new Person("Emily", "Clark", 25));
-        people.add(new Person("Michael", "Smith", 30));
-        people.add(new Person("Sarah", "Brown", 28));
-        people.add(new Person("David", "Lee", 35));
-        people.add(new Person("Olivia", "Taylor", 27));
-        people.add(new Person("James", "Wilson", 33));
-        people.add(new Person("Sophia", "Davis", 21));
-        people.add(new  Person("Ethan", "Martinez", 24));
-        people.add(new Person("Isabella", "Anderson", 26));
+        List<Person> people = Arrays.asList(
+        new Person("Richard", "John", 22),
+        new Person("Emily", "Clark", 25),
+        new Person("Michael", "Smith", 30),
+        new Person("Sarah", "Brown", 28),
+        new Person("David", "Lee", 35),
+        new Person("Olivia", "Taylor", 27),
+        new Person("James", "Wilson", 33),
+        new Person("Sophia", "Davis", 21),
+        new  Person("Ethan", "Martinez", 24),
+        new Person("Isabella", "Anderson", 26)
+        );
+//        String letter = scanner.nextLine();
+//        List<Person> nameFound = people.stream()
+//                        .filter(peoples -> people.getFirst().equalsI)
+//                                .collect(Collectors.toList());
 
         System.out.println("Enter a name to search (first or last)");
 
         String nameSearch = scanner.nextLine().trim();
 
-        List<Person> matches = new ArrayList<>();
+        List<Person> matches = people.stream()
+                .filter(ppl -> ppl.getFirstName().equalsIgnoreCase(nameSearch)
+                || ppl.getLastName().equalsIgnoreCase(nameSearch))
+                .collect(Collectors.toList());
+        
 
-        for( Person person : people){
-            if (person.getFirstName().equalsIgnoreCase(nameSearch)
-                    || person.getLastName().equalsIgnoreCase(nameSearch)) {
-                matches.add(person);
-            }
-        }
+//        for( Person person : people){
+//            if (person.getFirstName().equalsIgnoreCase(nameSearch)
+//                    || person.getLastName().equalsIgnoreCase(nameSearch)) {
+//                matches.add(person);
+//            }
+//        }
 
         if(matches.isEmpty()){
             System.out.println("This person was not found");
